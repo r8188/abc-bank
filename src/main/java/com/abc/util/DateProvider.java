55f914dd4167a6257112908dc8dcb6b1c9e50929
r8188 <rickylis@yahoo.com>
2016-01-21 22:08:11 -0500

    added private constructor

--- src/main/java/com/abc/util/DateProvider.java
+++ src/main/java/com/abc/util/DateProvider.java
@@ -6,6 +6,9 @@ import java.util.Date;
 public class DateProvider {
 	private volatile static DateProvider instance = null;
 
+	private DateProvider() {
+	}
+
 	public static DateProvider getInstance() {
 		if (instance == null) {
 			synchronized (DateProvider.class) {
